95bbb9b4fc0f5de9d28b0ea3f2bfbb2bd0c6b7c9
a1633661 <oscarsalem.garcia@student.adelaide.edu.au>
2017-08-09 13:37:22 +0800

    first commit

---
+++ src/main/java/com/oscar/boot/Application.java
@@ -0,0 +1,32 @@
+package com.oscar.boot;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+ 
+@RestController
+@SpringBootApplication
+//@Import(MyConfiguration.class)
+public class Application {
+
+	@Value("${name}")
+	String name;
+	
+	@Autowired
+	String message;
+	
+	
+	@RequestMapping("/")
+	public String home(){
+		return message;
+	}
+	
+	
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
